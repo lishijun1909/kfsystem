89851a178bc3394af54c04a036788bd16dd09503
qifendi <soncy1900@gmail.com>
2013-10-26 14:10:32 +0800

    2000W开房信息csv查询系统，不含数据
    查询自己的开房记录

---
+++ kfsearch.js
@@ -0,0 +1,23 @@
+/*
+ * Created with Sublime Text (buidl 3054).
+ * User: soncy
+ * Date: 2013-10-26
+ * Time: 12:21:43
+ * Contact: soncy1986@gmail.com
+ */
+
+var rd = require('rd'),
+    reader = require('./lib/reader.js');
+
+var files = rd.readSync('data');
+files.shift();
+
+/*
+ * row[Object], csv title lowercase
+ * index[Number]
+ */
+reader.read(files, function(row, index) {
+    
+}).on('end', function() {
+    console.log('data end');
+});
